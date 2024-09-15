69758b9b3a45a5a3f0e5f66aaf108388138d13c4
TuanDeng <nhovsxui@gmail.com>
2024-09-16 00:50:50 +0700

    init project

---
+++ lib/view_model/all_songs_view_model.dart
@@ -0,0 +1,249 @@
+import 'package:get/get.dart';
+
+class AllSongsViewModel extends GetxController {
+    final allList = [
+        
+      {
+        "id": "csaEsVWV",
+        "name": "Tại Vì Sao",
+        "type": "",
+        "album": "99%",
+        "year": "2011",
+        "releaseDate": null,
+        "duration": "473",
+        "label": "",
+        "artists": "MCK",
+        "artistsId": "456269, 455926, 455124",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "32891706",
+        "language": "hindi",
+        "hasLyrics": "true",
+        "url": "https://www.jiosaavn.com/song/kun-faaya-kun/ExsKdAdmYGU",
+        "copyright": "©  2011 ",
+        "image": "https://i.ytimg.com/vi/sYSvC8tOgKE/maxresdefault.jpg",
+        "downloadUrl": "https://rr7---sn-8qj-nboll.googlevideo.com/videoplayback?expire=1721997452&ei=LESjZuKLNKSsvdIPkrehyA8&ip=197.156.122.113&id=o-AD8alXS8WLU-EmgyzUzD0NnEBjBIw2fkjxjB4dk6OugR&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&bui=AXc671LNi0lvwhzHkfctGv_SFgekW94upZclQcJ5u0a49a_2uXa62qvw7CimROoRhvN4ipPjSS8Wq7oM&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3903385&dur=231.561&lmt=1714654376889985&keepalive=yes&c=ANDROID_CREATOR&txp=4502434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAJhAAwoXE2wwlbKc5PFklNYqEf25uVwyYWAStfgpFQP7AiEArNyZDVbuADN5exBLkzKtlk7cnMsuqtzqYb-e0mYKTJA%3D&rm=sn-xuj-5qq67r,sn-4wgr7z&rrc=79,104&fexp=24350516,24350517,24350556&req_id=1dba44029786a3ee&cmsv=e&redirect_counter=2&cms_redirect=yes&ipbypass=yes&mh=tL&mip=2001:ee0:4fb1:ad30:31ed:a7d3:ca24:85c3&mm=29&mn=sn-8qj-nboll&ms=rdu&mt=1721975598&mv=m&mvi=7&pl=46&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AGtxev0wRAIgRBmWBPWC4ExfT1CCd847FteEVdApHS6M9DgMr-N2bBoCIELs7zMeD42kD3nnSs1mYm0mJHtkqt0lkqlbCSdhFCCk"
+      },
+      {
+        "id": "1beIJP9H",
+        "name": "Từng Là",
+        "type": "",
+        "album": "",
+        "year": "2022",
+        "releaseDate": null,
+        "duration": "186",
+        "label": "Prime Digital Arena",
+        "artists": "Vũ Cát Tường",
+        "artistsId": "11294502",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "7777",
+        "language": "urdu",
+        "hasLyrics": "false",
+        "url": "https://www.jiosaavn.com/song/kun-faya-kun/QQoOeD5gDns",
+        "copyright": "(P) 2022 Prime Digital Arena",
+        "image": "https://photo-resize-zmp3.zadn.vn/w600_r1x1_jpeg/cover/1/6/1/d/161de9d6b8416b7f31e619d879c0cffd.jpg",
+        "downloadUrl": "https://rr4---sn-i3belnl6.googlevideo.com/videoplayback?expire=1722002748&ei=3FijZqfiIZCW1d8P366OSA&ip=2402%3A800%3A6311%3Acddb%3Ae98e%3A86cf%3Af782%3Ac2e5&id=o-AJ7HCV_hEmmlcCoMnSUumZC8Z8BsrnuNUWqCUuzIf2OG&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&gcr=vn&bui=AXc671IEWTGSzywjQPAK-5a7-noqHWf0gK5weZlvsJvd-g1JoIwuY7S4Tp-88wQFmQRyPRo323KQ9PO8&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=4570170&dur=276.121&lmt=1714782962895692&keepalive=yes&c=ANDROID_CREATOR&txp=4502434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAIC5oEuMGnMaV1gvgFjcrbyLFco432tJVwKNT1TOD_p1AiBe_M644E9WGMJAxsetTzcfVCrTk4BDBtwaW3z1D7Sh3g%3D%3D&rm=sn-8pxuuxa-nbosd7l,sn-8pxuuxa-nbo6r7l,sn-npo6d76&rrc=79,79,104&fexp=24350516,24350517,24350534,24350557&req_id=4cb566e707e3a3ee&redirect_counter=3&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=CI&mip=2001:ee0:4fb1:ad30:31ed:a7d3:ca24:85c3&mm=30&mn=sn-i3belnl6&ms=nxu&mt=1721984686&mv=m&mvi=4&pl=46&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AGtxev0wRgIhAP0wYbwXjlCaFOGt9Fr2B3wXV9znVXlHrg6XTXle0dFWAiEA2EnOLzdvFMvZAu77j_FZrOYXY8wiqQjmOi-TvZIkvAY%3D"
+      },
+      {
+        "id": "-Rb5lPKJ",
+        "name": "Đừng Làm Trái Tim Anh Đau",
+        "type": "",
+        "album": "",
+        "year": "2019",
+        "releaseDate": null,
+        "duration": "284",
+        "label": "PT Artha Musik Produksi",
+        "artists": "Sơn Tùng M-TP",
+        "artistsId": "3084476",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "15803",
+        "language": "indonesian",
+        "hasLyrics": "false",
+        "url": "https://www.jiosaavn.com/song/kun-faya-kun/XToJBBhgfHk",
+        "copyright": "(P) 2019 Artha Music Production",
+        "image": "https://i.scdn.co/image/ab67616d0000b273a1bc26cdd8eecd89da3adc39",
+        "downloadUrl": "https://rr6---sn-8qj-nbo66.googlevideo.com/videoplayback?expire=1722002894&ei=bVmjZqOVOryAjuMPjJOBwQQ&ip=183.81.152.65&id=o-AO6727i4LEhsgiBALFq1M4GgxG-3CNUqduPS8cHbGEHq&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&bui=AXc671LlS1BO_UKvex36ZJ0l7OfE0hfDJKZyrF2_u9dLAnpycYImWSSMH1dDXhW72Bjf6sL9ED2zlVAz&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=4782520&dur=279.381&lmt=1720361802349097&keepalive=yes&c=ANDROID_CREATOR&txp=5532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAI5KNF3Qpy-XEavY3jmRBEXripSRF-62AF3SiflNZaAuAiEA-y0ADYi4cPpL9RQHLtdhGdRsdxmxh4dFrh4KNdbmPFM%3D&rm=sn-h5ugpm-jb367s,sn-npo6z7l&rrc=79,104&fexp=24350516,24350518,24350557&req_id=f1be52deea81a3ee&redirect_counter=2&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=zx&mip=2001:ee0:4fb1:ad30:31ed:a7d3:ca24:85c3&mm=29&mn=sn-8qj-nbo66&ms=rdu&mt=1721980873&mv=m&mvi=6&pl=46&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AGtxev0wRgIhAI0KOQSc3bSPNaGjcJSo7kOmc4jK6fXCa5k6q532D9X_AiEAhRlTtIp3szPTR3DCM1YAyld5cHy5ItaSqX38BbAR4VY%3D"
+      },
+      {
+        "id": "brqxXg-c",
+        "name": "Anh Là Ngoại Lệ Của Em",
+        "type": "",
+        "album": "Kun Faya Kun",
+        "year": "2023",
+        "releaseDate": null,
+        "duration": "255",
+        "label": "Prime Digital Arena",
+        "artists": "Phương Ly",
+        "artistsId": "10209176",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "160",
+        "language": "urdu",
+        "hasLyrics": "false",
+        "url": "https://www.jiosaavn.com/song/kun-faya-kun/EhoaSSxXGlA",
+        "copyright": "(P) 2023 Prime Digital Arena",
+        "image": "https://i.ytimg.com/vi/q2YUtZum9wc/maxresdefault.jpg",
+        "downloadUrl": "https://rr3---sn-i3belnls.googlevideo.com/videoplayback?expire=1722002769&ei=8VijZrCgHvm4rtoPsJmHwAM&ip=2402%3A800%3A6311%3Acddb%3Ae98e%3A86cf%3Af782%3Ac2e5&id=o-AAaO-vo5miUblEkENArG3tc7tD9Ohqwzwz-OZhZu1Pwf&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&gcr=vn&bui=AXc671IQYIjAUTdYeAQwsG6NbCp1YqOsgMj4I8t-HY0MvZBOvkAmkzSy-3wDOllfpVXOgATWTPiAp4cd&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3608556&dur=218.241&lmt=1714860816347550&keepalive=yes&c=ANDROID_CREATOR&txp=4502434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAINGvxjDkGb1xujC-1wzcdXljGb4IBYuMXG1qV3QIHRsAiBR7hd1ebSscjEcSkVELYYdOKum0l-q02EUcjUgsgwyJA%3D%3D&rm=sn-8pxuuxa-nboz67e,sn-8pxuuxa-nbo6s7z,sn-npolr7l&rrc=79,79,104&fexp=24350516,24350517,24350534,24350557&req_id=4c4dbfd0c80ca3ee&redirect_counter=3&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=WW&mip=2001:ee0:4fb1:ad30:31ed:a7d3:ca24:85c3&mm=30&mn=sn-i3belnls&ms=nxu&mt=1721980844&mv=m&mvi=3&pl=46&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AGtxev0wRAIgMtognodlL5rSwCBak_WaBnLyvDgHt15VjFikR_0zQkUCICw-yVyiU4Ctb917Q3cmRPPwWhBvVT1iqK7GCntWD01k"
+      },
+      {
+        "id": "l56idbSQ",
+        "name": "có hẹn với thanh xuân",
+        "type": "",
+        "album": "",
+        "year": "2022",
+        "releaseDate": null,
+        "duration": "195",
+        "label": "Hunter Prince Khalid",
+        "artists": "SUNI Hạ Linh, Hoàng Dũng, GREY D, Orange, tlinh",
+        "artistsId": "10808504",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "686",
+        "language": "unknown",
+        "hasLyrics": "false",
+        "url": "https://www.jiosaavn.com/song/kun-faya-kun/HF1dWBBSZGI",
+        "copyright": "℗ 2022 Hunter Prince Khalid",
+        "image": "https://i.ytimg.com/vi/vpRi8S6uXAg/maxresdefault.jpg",
+        "downloadUrl": "https://rr5---sn-i3bssn7e.googlevideo.com/videoplayback?expire=1722006823&ei=x2ijZp6tMNS52roP5Ki08Ao&ip=2405%3A4802%3A9165%3A8db0%3Ad446%3Afedd%3A5299%3A5355&id=o-ALk7M535TfbigWuS-zQt_D3xp5lZiR3w0jDoadyOMFTi&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&gcr=vn&bui=AXc671I3WKXZnTT5aGLQ1akwI4NhT0Q5D8fwIdcauHjYnsxk3V3eAPHmgTMuTQLzjJ-Y90FXPpJMxgLp&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3665433&dur=218.041&lmt=1714563748023393&keepalive=yes&c=ANDROID_CREATOR&txp=2318224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAJ_0MARqDwV36z0bbR6vrhhKE4rE0x9v-bR4o_rmtAJ2AiEAtR3bJYbaU68bqJyOraPzPikiek3vptKCB1dY_ovjy0Q%3D&rm=sn-42u-nbozz7d,sn-i3bk776&rrc=79,104&fexp=24350516,24350517,24350557&req_id=cebe3c59096da3ee&redirect_counter=2&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=yn&mip=2001:ee0:4fb1:ad30:31ed:a7d3:ca24:85c3&mm=30&mn=sn-i3bssn7e&ms=nxu&mt=1721984925&mv=m&mvi=5&pl=46&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AGtxev0wRQIhAKWtIhnl9d9K1Bxiacb0BjH5pAMXQaBWflao2g0J_2sSAiBwW_a-GvQ4UKngCN2BovB5QAk8N46AOu8IZFq-bS-ziA%3D%3D"
+      },
+      {
+        "id": "FJx1Tj5w",
+        "name": "Ngày Đẹp Trời Để Nói Chia Tay",
+        "type": "",
+        "album": "",
+        "year": "2021",
+        "releaseDate": null,
+        "duration": "94",
+        "label": "Fita Cassete",
+        "artists": "Lou Hoàng",
+        "artistsId": "8699736",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "294",
+        "language": "unknown",
+        "hasLyrics": "false",
+        "url": "https://www.jiosaavn.com/song/kun-faya-kun/NiITACBaAkQ",
+        "copyright": "℗ 2021 Fita Cassete",
+        "image": "https://i.ytimg.com/vi/0xAW6MAT_Wo/maxresdefault.jpg",
+        "downloadUrl": "https://rr6---sn-42u-i5old.googlevideo.com/videoplayback?expire=1721548885&ei=9WucZtfQDLj32roPsIv40As&ip=1.53.53.211&id=o-AMkYpLEWpPeANNLkLAlGVLRyGDVdyxI2QWHNy3oPIWmq&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=vV&mm=31%2C26&mn=sn-42u-i5old%2Csn-un57enez&ms=au%2Conr&mv=m&mvi=6&pl=24&gcr=vn&initcwndbps=2271250&bui=AXc671JVpErTWyoqjWPe-8M1CVXClgi7GdM0hYC-Y11hi6extPKlYjqSaKhg4EJE0eKtyCjRDn_XvCGp&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3804243&dur=230.901&lmt=1719481296947633&mt=1721526767&fvip=3&keepalive=yes&c=ANDROID_CREATOR&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAPG4VqgG03RnAYIgHzgvEfk-UXCKrQuMS-f138B6rMoaAiAYIh4KqEipFvdWpJ9wzamBzhsNv3B_YOLXYHLO8qkweA%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AHlkHjAwRAIgLgod4bHb7yedpF1ICri4ekh9TPtRJGklpuI5RU66vRcCIAuEsI_8aZqm3-UMtmZ6FLjsbAvumyYbN2qxZJ-AZ81o"
+      },
+      {
+        "id": "_Go9q0V3",
+        "name":
+            "Trái đất ôm Mặt trời",
+        "type": "",
+        "album": "",
+        "year": "2010",
+        "releaseDate": null,
+        "duration": "279",
+        "label": "Green Dome Edu-Tainment",
+        "artists": "Kai Đinh, GREY D, Hoàng Thùy Linh",
+        "artistsId": "2898666",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "728",
+        "language": "english",
+        "hasLyrics": "false",
+        "url":
+            "https://www.jiosaavn.com/song/kun-fa-ya-kun-be-and-it-will-be-feat.-al-ghazali-college-choir/Ly8ECAUAYQA",
+        "copyright": "© 2010 Hussein Kalla",
+        "image": "https://photo-resize-zmp3.zadn.vn/w600_r1x1_jpeg/banner/4/a/e/1/4ae1d8230755ee0fbe1acff06048324e.jpg",
+        "downloadUrl": "https://rr1---sn-42u-nbozl.googlevideo.com/videoplayback?expire=1721555265&ei=4YScZuzqD-LCrtoPh7O1sAg&ip=202.29.234.109&id=o-AJ3mobqibq4Nv2cXRAyYBAm8TwIfURWatPgRGCPxhvEC&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&bui=AXc671JEAtagpO1xDMtCJPftVHABittcrhpIqGQQdNA8B66gm3nBEn-wTg-kh1TFSqm6O9hPJeqZjitM&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3395262&dur=207.021&lmt=1720591503723616&keepalive=yes&c=ANDROID_CREATOR&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAJtULg8mGgKS339oSs3vM1HTsG1yyFel4YF7_KEKaoXaAiEA-IOz5D88MUZHp_2q8zcu617uXCdu0TrL5B8dedQvQE8%3D&rm=sn-0opoxuui5pa5oq-c33d7e,sn-npozl76&rrc=79,104&fexp=24350516,24350518&req_id=2ae1ef98bc6aa3ee&redirect_counter=2&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=oq&mip=2405:4802:80b1:7800:c902:303e:c9c4:e681&mm=29&mn=sn-42u-nbozl&ms=rdu&mt=1721533245&mv=m&mvi=1&pl=44&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRgIhAKrWy0aTl2Av7srHAz-wwoe3aVcfnO526ErbvTnJ9vZgAiEAqeb2wSwIroFC1_CEmOiEp8cOj-BZUIkGF-pWgNnEGuk%3D"
+      },
+      {
+        "id": "ALPzy3Lp",
+        "name": "Lặng",
+        "type": "",
+        "album": "",
+        "year": "2021",
+        "releaseDate": null,
+        "duration": "293",
+        "label": "Aqsa Abdul Haq",
+        "artists": "Rhymastic",
+        "artistsId": "2020208",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "143",
+        "language": "urdu",
+        "hasLyrics": "false",
+        "url": "https://www.jiosaavn.com/song/kun-faya-kun/MSQ7Sw0De0M",
+        "copyright": "© 2021 Aqsa Abdul Haq",
+        "image": "https://bloganchoi.com/wp-content/uploads/2024/07/loi-bai-hat-lang-rhymastic-1.jpg",
+        "downloadUrl": "https://rr6---sn-42u-nbozz.googlevideo.com/videoplayback?expire=1721555302&ei=BoWcZvmjG72jvdIP7vKM-AM&ip=185.188.254.2&id=o-AP8rrWaaywYKbyVnIremkFV6Y9b_lZECWbJ16DzPaOV_&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&bui=AXc671JOObaiEDn27qjMGxVU-KqQijK1ILLLYq50WrhDVaMXlMfJDI7T2FMpzt-9NZw7jY3-3CL-1r7e&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=2508823&dur=164.561&lmt=1720470954722261&keepalive=yes&c=ANDROID_CREATOR&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAI0UKRgz3dDqoGMTzYUDTcEED0g1oahxXvJ2CFZxW6YLAiEAvBf1ycXRnZn9GyAw1G25jLkn1tGSzy9IgEsVGPJpiXc%3D&redirect_counter=1&rm=sn-25grz76&rrc=104&fexp=24350516,24350517&req_id=20b2e31e788ca3ee&cms_redirect=yes&ipbypass=yes&mh=lh&mip=2405:4802:80b1:7800:c902:303e:c9c4:e681&mm=31&mn=sn-42u-nbozz&ms=au&mt=1721533246&mv=m&mvi=6&pl=44&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIgCWszJWYAT47K1-LFT1HhC48kDuhk7UNKwb7Dmpsc45QCIQCuvMSStpK81BBXkg3-mrcbXKCoCc3esktuakTv3bbNUA%3D%3D"
+      },
+      {
+        "id": "vQDUtZvr",
+        "name": "Từng Quen",
+        "type": "",
+        "album": "LOI CHOI",
+        "year": "2021",
+        "releaseDate": null,
+        "duration": "298",
+        "label": "Meem Production",
+        "artists": "Wren Evans",
+        "artistsId": "889539",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "132",
+        "language": "urdu",
+        "hasLyrics": "false",
+        "url": "https://www.jiosaavn.com/song/kun-faya-kun/BjkvZABqQUE",
+        "copyright": "(P) 2021 Smart Tech",
+        "image":
+          "https://i.ytimg.com/vi/zepHPnUDROE/maxresdefault.jpg",
+        "downloadUrl": "https://rr4---sn-42u-nbosk.googlevideo.com/videoplayback?expire=1721555346&ei=MoWcZqOZLKnGy_sP0-TCuAU&ip=2604%3Ab000%3Ab218%3A1aa6%3Ad5d3%3Aa6ea%3Ad395%3A14c6&id=o-AB0T8q3NG0Zp78cHNiCgCjsbHIzq-zc5iF4onqPA_OFQ&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&bui=AXc671JVLw4rrKZ47fH1aXcz98jvsYXKmHNmLAdTeGD2UwwRSkF_xQittG9yPBL4fWtWhi82Ym4R7sh2&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=2999703&dur=174.581&lmt=1714926298574367&keepalive=yes&c=ANDROID_CREATOR&txp=4502434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRAIgMLAbiUgqMdeOjcN4wWE05hxfCdF3fo8m2zh2-MT3MWMCIDtnoguFKc_fG7Hc8T3QUz_GXPJ-J2gUPPLAYbdiW1nX&rm=sn-apcxgutj5cax2g-nw067e,sn-hp5rr7z&rrc=79,104&fexp=24350516,24350517&req_id=527714d30cc5a3ee&cmsv=e&redirect_counter=2&cms_redirect=yes&ipbypass=yes&mh=sA&mip=2405:4802:80b1:7800:c902:303e:c9c4:e681&mm=29&mn=sn-42u-nbosk&ms=rdu&mt=1721533484&mv=m&mvi=4&pl=44&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRgIhALSJ8XGLMTEXFEZKEGvdkqpLHKX3N2qoHYRTb48c1aLbAiEAv2lc85S_8qJlnKwzfjw5zEdy30OlDf2JPgmN1qj3aS0%3D"
+      },
+      {
+        "id": "9AHD4CaS",
+        "name": "Những Lời Hứa Bỏ Quên",
+        "type": "",
+        "album": "",
+        "year": "2023",
+        "releaseDate": null,
+        "duration": "210",
+        "label": "Mannan Multimedia",
+        "artists": "Vũ., Dear Jane",
+        "artistsId": "4540111",
+        "featuredArtists": "",
+        "featuredArtistsId": "",
+        "explicitContent": 0,
+        "playCount": "22",
+        "language": "bengali",
+        "hasLyrics": "false",
+        "url": "https://www.jiosaavn.com/song/kun-faya-kun/SSkjdUBzVmA",
+        "copyright": "℗ 2023 Mannan Multimedia",
+        "image": "https://avatar-ex-swe.nixcdn.com/song/2023/12/12/7/a/0/c/1702398577338_640.jpg",
+        "downloadUrl": "https://rr1---sn-42u-nboee.googlevideo.com/videoplayback?expire=1721546040&ei=2GCcZpKPJMK6juMPz6Ww-AY&ip=2402%3A800%3A6216%3Aecc6%3A14c8%3Afdf3%3A8c67%3A264a&id=o-AE-1ykj4iXHpShy8un5jtrMsG-Ku5Kp_0BOjSnviQILO&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&gcr=vn&bui=AXc671IK2ai9ww8n-0Vx8amWOmDT_4ZVEBHdqBvJq7wXAfZD04w7wHkwnqr_k8SMMj8AnrJAH-1i9VAi&vprv=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=4230780&dur=258.881&lmt=1714436662020617&keepalive=yes&c=ANDROID_CREATOR&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cvprv%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAK5ItcxBGjEXM6F9U3VxXT1djDrLEvCsBjQK6K-AGiY5AiBc1vVMDh7dIiedoHVTFyjHSs6JFBGn8l88mTnrBL8kKw%3D%3D&rm=sn-8pxuuxa-jmg67s,sn-i3bke7z&rrc=79,104&fexp=24350516,24350517&req_id=22e37339b5dba3ee&redirect_counter=2&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=4q&mip=2405:4802:80b1:7800:c902:303e:c9c4:e681&mm=29&mn=sn-42u-nboee&ms=rdu&mt=1721533484&mv=m&mvi=1&pl=44&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIhAMPrk9QHlQlqk3-hLfV-dBJGWaaPbEo-XQw8E_FKASJzAiB--Mawla5hfYl4kX02GKJlevUmxthH56bqQuiHIDGfbg%3D%3D"
+      }
+    
+    ].obs;
+     final filteredList = <Map<String, dynamic>>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    filteredList.value = allList;
+  }
+
+  void searchSongs(String query) {
+    if (query.isEmpty) {
+      filteredList.value = allList;
+    } else {
+      filteredList.value = allList.where((song) {
+        return song['name'].toString().toLowerCase().contains(query.toLowerCase()) ||
+               song['artists'].toString().toLowerCase().contains(query.toLowerCase());
+      }).toList();
+    }
+  }
+}
